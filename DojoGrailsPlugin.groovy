bc827bc10cc2cb900b53f7df25c2d7ab763d9424
Rob Meidal <rob@netapplogic.com>
2010-10-05 12:37:52 -0600

    Updated readme and version number to 1.4.3.1

--- DojoGrailsPlugin.groovy
+++ DojoGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import org.codehaus.groovy.grails.plugins.web.taglib.*
 
 class DojoGrailsPlugin {
-  def version = "1.4.3"
+  def version = "1.4.3.1"
   def grailsVersion = "1.3.0 > *"
   def dependsOn = [:]
   def pluginExcludes = [
@@ -21,6 +21,10 @@ class DojoGrailsPlugin {
   def description = """
     Adds Dojo Base as a Javascript Provider for Grails. This provides full support for
     <g:formRemote>, <g:remoteField>, <g:remoteFunction>, <g:remoteLink> and <g:submitToRemote>.
+
+    Also adds two dojo tags: <dojo:paginate> and <dojo:sortableColumn>. These do the same thing
+    that the grails versions of the tags but will do them via ajax calls. 
+
     For more information about the Dojo Toolkit please visit http://www.dojotoolkit.org/.
   """  
   def documentation = "http://grails.org/plugin/dojo"
