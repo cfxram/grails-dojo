b46ab373ea05a353987b057ecc9b4fd1b9d2aa35
Rob Meidal <rob@netapplogic.com>
2010-11-29 16:22:59 -0700

    Added custom javascript dojo files to the <dojo:header> tag.

--- grails-app/taglib/org/dojotoolkit/DojoTagLib.groovy
+++ grails-app/taglib/org/dojotoolkit/DojoTagLib.groovy
@@ -1,17 +1,55 @@
 package org.dojotoolkit
 
+import grails.converters.deep.JSON
 
 
 class DojoTagLib {
   static namespace = 'dojo'
-  def DOJO_HOME = "${g.resource(dir:pluginContextPath)}/js/dojo/${Dojo.version}"
+  def DOJO_HOME = "${g.resource(dir: pluginContextPath)}/js/dojo/${Dojo.version}"
   def CUSTOM_DOJO = "${g.resource()}/js/dojo/${Dojo.version}-custom"
-  
 
-	private boolean hasCustomDojoBuild() {
-		return application.config.uiperformance.enabled
-	}  
+  /**
+   * Returns the dojo.customBuild value from Config.groovy
+   * @return
+   */
+  private boolean hasCustomDojoBuild() {
+    return grailsApplication.config.dojo.customBuild
+  }
 
+  /**
+   * Reads the dojo.profile.js file and converts into a grails object
+   * @return JSONObject
+   */
+  def getJSONProfile() {
+    def jsonString = new File("grails-app/conf/dojo.profile.js").text;
+    def jsonObj = JSON.parse("{${jsonString}}");
+    return jsonObj.dependencies
+  }
+
+  /**
+   * Will return the dojo home based on if it has a custom build
+   * @return String
+   */
+  def dojoHome() {
+    if (hasCustomDojoBuild()) {
+      return CUSTOM_DOJO
+    }
+    else {
+      return DOJO_HOME
+    }
+  }
+
+  /**
+   * Will output custom js scripts that were created as part of the custom dojo build.
+   */
+  def customDojoLayers = {
+    if (hasCustomDojoBuild()) {
+      def dependencies = getJSONProfile()
+      dependencies.layers.each {
+        out << "<script type='text/javascript' src='${dojoHome()}/dojo/${it.name}'></script>"
+      }
+    }
+  }
 
   /**
    * Alternative to <g:javascript library="dojo"/>. This will include the dojo.js file,
@@ -19,23 +57,29 @@ class DojoTagLib {
    *
    * @params attrs.require = This is a map of components to include
    * @params attrs.theme = (optional) Will include the theme if it is provided
+   * @params attrs.customDojo = (true) Will include the js files(layers) from dojo.profile.js
    */
   def header = {attrs ->
     def debug = attrs.remove("debug") ?: "false"
     def parseOnLoad = attrs.remove("parseOnLoad") ?: "true"
-    if(attrs?.theme){
-      out << stylesheets(attrs)  
+    def customDojo = attrs.remove("customDojo") ?: true
+
+    if (attrs?.theme) {
+      out << stylesheets(attrs)
     }
-    out << "<script type='text/javascript' src='${DOJO_HOME}/dojo/dojo.js' djConfig='isDebug:${debug}, parseOnLoad:${parseOnLoad}'></script>"
-    if(attrs?.modules){
+
+    out << "<script type='text/javascript' src='${dojoHome()}/dojo/dojo.js' djConfig='isDebug:${debug}, parseOnLoad:${parseOnLoad}'></script>"
+
+    // if custom build then include released js files
+    if(customDojo){
+      out << customDojoLayers()
+    }
+    
+    if (attrs?.modules) {
       out << require(attrs)
     }
-
-    // If there is a custom dojo created... then include the correct js files here.
-
   }
 
-  
   /**
    * Will setup the base css and themes.User still needs to define <body class="${theme}">
    *
@@ -44,26 +88,25 @@ class DojoTagLib {
   def stylesheets = {attrs ->
     def theme = attrs.remove("theme") ?: "tundra"
     out << """
-      <link rel="stylesheet" type="text/css" href="${DOJO_HOME}/dojo/resources/dojo.css" />
-      <link rel="stylesheet" type="text/css" href="${DOJO_HOME}/dijit/themes/dijit.css" />
-      <link rel="stylesheet" type="text/css" href="${DOJO_HOME}/dijit/themes/${theme}/${theme}.css" />
+      <link rel="stylesheet" type="text/css" href="${dojoHome()}/dojo/resources/dojo.css" />
+      <link rel="stylesheet" type="text/css" href="${dojoHome()}/dijit/themes/dijit.css" />
+      <link rel="stylesheet" type="text/css" href="${dojoHome()}/dijit/themes/${theme}/${theme}.css" />
     """
   }
 
-  
   /**
    * Will include dojo modules via the dojo loader.
    * @params attrs.modules = This is a map of components to include 
    */
   def require = {attrs ->
     // Add Required attributes
-    if(attrs.modules){
+    if (attrs.modules) {
       out << "<script type='text/javascript'>"
     }
-    attrs.modules.each{
+    attrs.modules.each {
       out << "dojo.require('${it}');"
     }
-    if(attrs.modules){
+    if (attrs.modules) {
       out << "</script>"
     }
   }
