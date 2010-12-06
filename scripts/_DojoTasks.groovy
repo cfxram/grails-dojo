1bc9034fd711c0589c07547884932a6bb93c2ed3
Rob Meidal <rob@netapplogic.com>
2010-12-06 13:35:33 -0700

    Fixed a small bug because of dojoui refactor.

--- scripts/_DojoTasks.groovy
+++ scripts/_DojoTasks.groovy
@@ -29,10 +29,6 @@ target(downloadDojoSource: "This will download the source version of Dojo.") {
   move(todir:tmpWorkingDir){
       fileset(dir: "${downloadDir}/dojo-release-${version}-src", includes: "**/**")    
   }
-  // Copy DojoUI files
-  copy(todir:"${tmpWorkingDir}/dojoui"){
-      fileset(dir: "${dojoPluginDir}/web-app/js/dojoui", includes: "**/**")
-  }
 }
 
 
