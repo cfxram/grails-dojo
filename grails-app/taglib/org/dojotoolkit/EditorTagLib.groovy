f130065013db9d1745143ed4dacf6cbf8d358d44
Rob Meidal <rob@netapplogic.com>
2011-12-21 16:52:45 -0700

    Updates to the dojo editor.

--- grails-app/taglib/org/dojotoolkit/EditorTagLib.groovy
+++ grails-app/taglib/org/dojotoolkit/EditorTagLib.groovy
@@ -9,7 +9,15 @@ class EditorTagLib {
    * Outputs the required javascript dojo libraries
    */
   def editorResources = {attrs,body ->
-    out << dojo.require(modules:['dijit.Editor','dijit._editor.plugins.LinkDialog','dijit._editor.plugins.FontChoice'])
+    out << dojo.require(modules:[
+            'dijit.Editor',
+            'dijit._editor.plugins.TextColor',
+            'dijit._editor.plugins.LinkDialog',
+            'dijit._editor.plugins.ViewSource',
+            'dijit._editor.plugins.FontChoice',
+            'dojox.editor.plugins.PasteFromWord'
+          ])
+    out << dojo.css(file:"dojox/editor/plugins/resources/css/PasteFromWord.css")
   }
 
 
@@ -18,33 +26,58 @@ class EditorTagLib {
    * Creates a dojo rich text editor with default values chosen.
    */
   def editor = {attrs, body ->
+    attrs.type = attrs?.type ?: "simple"   // Can be simple || intermediate || advanced
     attrs.height = attrs?.height ?: "150px"
     attrs.class =  (attrs?.class) ? "${attrs?.class} DojoUiEditor" : "DojoUiEditor"
     attrs.id = attrs.remove("elementId") ?: attrs?.id ?: attrs.remove("name") ?: "dojo_editor_${Util.randomId()}"
     def value = attrs.remove("value") ?: body()
 
-    /*
-      HACK - RM (12-20-2011)
-      Removing the 'dijit._editor.plugins.EnterKeyHandling' plugin because of these bugs:
+    def undo        = " 'undo', 'redo' "
+    def msPaste     = " 'pastefromword' "
+    def fontSimple  = " 'bold', 'italic', 'underline', 'strikethrough' "
+    def lists       = " 'insertOrderedList', 'insertUnorderedList' "
+    def indent      = " 'indent', 'outdent' "
+    def justify     = " 'justifyLeft', 'justifyRight', 'justifyCenter'"
+    def fontExtra   = " 'fontName', 'fontSize' "
+    def color       = " 'foreColor', 'hiliteColor' "
+
+    def defaultPlugins = " ${fontSimple}, '|', ${lists}, '|',  ${indent}, '|', ${justify}, '|', ${msPaste}"
+
+
+     /*
+      WARNING - RM (12-20-2011)
+      Don't use the "dijit._editor.plugins.EnterKeyHandling" plugin because of these bugs:
       http://bugs.dojotoolkit.org/ticket/13399
       http://bugs.dojotoolkit.org/ticket/13744
      */
-    def defaultPlugins = """
-      ['bold','italic','underline','|', 'insertOrderedList','insertUnorderedList','indent','outdent','|',
-      'justifyLeft','justifyCenter', '|', 'createLink', '|', {name:'fontSize', plainText: true}]
-    """
-    attrs.plugins = attrs?.plugins ?: defaultPlugins
+
+
+
+
+    // Define default sets of plugins
+    if(attrs?.type == "simple"){
+
+    }
+    else if(attrs?.type == "intermediate"){
+      defaultPlugins += ", '|', ${fontExtra}, '|','createLink','viewsource'"
+    }
+    else if(attrs?.type == "advanced"){
+      defaultPlugins = "${undo}, '|', ${defaultPlugins}, '|', ${color}, '|', 'createLink','viewsource' "
+    }
+
+    def plugins = attrs.remove("plugins") ?: defaultPlugins
 
     out << """
       <fieldset class="dojo-ui-editor">
-        <div dojoType="dijit.Editor" ${Util.htmlProperties(attrs)}>
-          <script type="dojo/connect" event="onDisplayChanged">
-            dojo.byId('${attrs?.id}').value = this.attr('value')
-          </script>
-          ${value}
+        <div data-dojo-type="dijit.Editor" ${Util.htmlProperties(attrs)} data-dojo-props="plugins:[${plugins}]">
+            <script type="dojo/connect" event="onDisplayChanged">
+              dojo.byId('${attrs?.id}').value = this.attr('value')
+            </script>
+            ${value}
         </div>
       </fieldset>
     """
+
   }
 
 }
--- grails-app/views/widget/editor.gsp
+++ grails-app/views/widget/editor.gsp
@@ -6,14 +6,42 @@
 </head>
 <body>
 
+<div style="float:right; margin-right:2em">
+  <h2>Simple (small):</h2>
+  <dojo:editor name="simpleExample-Small" style="width:400px; margin:0 0 0 2em ">
+    <div>Test</div><p>Test</p>
+  </dojo:editor>
+</div>
+
+<div  style="float:left; margin-left:1em">
+  <h2>Simple:</h2>
+  <dojo:editor name="simpleExample" style="width:600px; margin:0 0 0 2em ">
+    <div>Test</div><p>Test</p>
+  </dojo:editor>
+</div>
 
 
-<dojo:editor style="width:600px; margin: 3em;">
-  <div>
-    Test
-  </div>
-  <p>Test</p>
-</dojo:editor>
+<div style="float:right; margin-right:2em; clear:both">
+  <h2>Intermediate (small):</h2>
+  <dojo:editor type="intermediate" name="intermediateExampleSmall" style="width:400px; margin:0 0 0 2em ">
+    <div>Test</div><p>Test</p>
+  </dojo:editor>
+</div>
+
+<div style="float:left; margin-left:1em;">
+  <h2>Intermediate:</h2>
+  <dojo:editor type="intermediate" name="intermediateExample" style="width:600px; margin:0 0 0 2em ">
+    <div>Test</div><p>Test</p>
+  </dojo:editor>
+</div>
+
+
+<div style="float:left; margin-left:2em; clear:both">
+  <h2>Advanced:</h2>
+  <dojo:editor type="advanced" name="advanced" style="width:800px; margin:0 0 0 2em ">
+    <div>Test</div><p>Test</p>
+  </dojo:editor>
+</div>
 
 
 </body>
