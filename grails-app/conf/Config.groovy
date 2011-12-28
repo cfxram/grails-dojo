49aa7cce4eca88bbb770257bfcbad479521e79f5
Rob Meidal <rob@netapplogic.com>
2011-12-28 11:02:23 -0700

    Updated version number.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -35,7 +35,7 @@ dojo.use.customBuild.css = true; /* [DEPRICATED] use dojo.profile.css  */
  * Only Dojo Grails Plugin understand this. It will create a single css file from these
  * files. Each of these files will be @import'ed so that the dojo build process will
  * detect and combine them.
- */
+
 dojo.css = """
   dependencies: [
     "../dojo/resources/dojo.css",
@@ -45,12 +45,13 @@ dojo.css = """
     "../dojoui/resources/css/dojo-ui.css"
   ]
 """
+ */
 
 /**
  * Use for Dojo 1.7 and up...
  * New profile file format. See the livedocs for more information:
  * http://livedocs.dojotoolkit.org/build/index
- */
+
 dojo.profile = """
   var profile = {
     basePath: "../../web-app/js/dojoTmp",  //relative to grails-app/conf/
@@ -84,13 +85,14 @@ dojo.profile = """
     }
   };
 """
+ */
 
 /**
  * [DEPRICATED]
  * This is the old profile supported by Dojo 1.6.1 and under. For more info, see:
  * http://livedocs.dojotoolkit.org/build/pre17/build
  */
-dojo.oldprofile = """
+dojo.profile = """
 dependencies = {
     layers:  [
         {
