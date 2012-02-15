ee0f9c36704c28b347bfe0e1fccb66b5eaa74263
Rob Meidal <rob@netapplogic.com>
2012-02-15 08:53:31 -0700

    Tagging version number as dev version.

--- src/groovy/org/dojotoolkit/Dojo.groovy
+++ src/groovy/org/dojotoolkit/Dojo.groovy
@@ -2,7 +2,7 @@ package org.dojotoolkit
 
 class Dojo {
   static version = "1.6.1"
-  static pluginVersion = "1.6.1.6"
+  static pluginVersion = "1.6.1.6-DEV"
   static srcHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}-src.zip"
   static releaseHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}.zip"
 }
