6e23a6b6c22a7fd33efafb29d90a13511fb04481
Rob Meidal <rob@netapplogic.com>
2012-02-29 15:57:26 -0700

    Updated version number right before release.

--- DojoGrailsPlugin.groovy
+++ DojoGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import org.dojotoolkit.*
 import org.codehaus.groovy.grails.plugins.web.taglib.JavascriptTagLib
 // Uncompressed 
 class DojoGrailsPlugin {
-  def version = "1.6.1.7-DEV"
+  def version = "1.6.1.7"
   def grailsVersion = "1.3.0 > *"
   def dependsOn = [:]
   def pluginExcludes = [
