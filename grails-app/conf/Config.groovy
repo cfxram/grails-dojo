a2bbb64f6b921f09b1972bcba96ffea98e6f6f72
Rob Meidal <rob@netapplogic.com>
2012-06-19 13:38:30 -0600

    Minor updates to config files. Updated version num to minor .12

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,15 +1,7 @@
-// configuration for plugin testing - will not be included in the plugin zip
-
 grails.views.javascript.library="dojo"
-
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
@@ -21,7 +13,6 @@ log4j = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
-
     warn   'org.mortbay.log'
 }
 
@@ -93,11 +84,6 @@ dependencies = {
 }
 """
 
-
-
-
-
-
 environments {
   development {
     dojo.optimize.during.build = false;
@@ -106,9 +92,3 @@ environments {
   }
 }
 
-
-grails.release.scm.enabled = false
-
-
-grails.views.default.codec="none" // none, html, base64
-grails.views.gsp.encoding="UTF-8"
--- src/groovy/org/dojotoolkit/Dojo.groovy
+++ src/groovy/org/dojotoolkit/Dojo.groovy
@@ -2,7 +2,7 @@ package org.dojotoolkit
 
 class Dojo {
   static version = "1.6.1"
-  static pluginVersion = "1.6.1.11"
+  static pluginVersion = "1.6.1.12-DEVa"
   static srcHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}-src.zip"
   static releaseHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}.zip"
 }
