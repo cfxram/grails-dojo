f862225e35c75b28e9e56d6e14d1d9f7c9100948
Rob Meidal <rob@netapplogic.com>
2012-12-04 11:14:02 -0700

    Added place for show more functionality.

--- src/groovy/org/dojotoolkit/Dojo.groovy
+++ src/groovy/org/dojotoolkit/Dojo.groovy
@@ -2,7 +2,7 @@ package org.dojotoolkit
 
 class Dojo {
   static version = "1.6.1"
-  static pluginVersion = "1.6.1.12"
+  static pluginVersion = "1.6.1.13-DEVa"
   static srcHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}-src.zip"
   static releaseHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}.zip"
 }
