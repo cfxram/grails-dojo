cf6dbbbd03e7cf6d6786f99c6ec36f39828757ae
Rob Meidal <rob@netapplogic.com>
2013-01-23 17:42:28 -0700

    Updated version number - Read for release

--- src/groovy/org/dojotoolkit/Dojo.groovy
+++ src/groovy/org/dojotoolkit/Dojo.groovy
@@ -2,7 +2,7 @@ package org.dojotoolkit
 
 class Dojo {
   static version = "1.6.1"
-  static pluginVersion = "1.6.1.16-DEVa"
+  static pluginVersion = "1.6.1.16"
   static srcHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}-src.zip"
   static releaseHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}.zip"
 }
