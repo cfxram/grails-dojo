e714949510f233c60bef35bda3dda000231a79ae
Jonathan Hawk <jonathan@libreworks.com>
2013-05-28 10:43:41 -0400

    Upgrading to Grails 2.2.x

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -9,15 +9,15 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" 
+	legacyResolve false
+	pom true
     repositories {
+		mavenLocal()
         grailsPlugins()
         grailsHome()
         grailsCentral()
     }
     dependencies {}
     plugins {
-      build   ":tomcat:$grailsVersion"
-      compile ":hibernate:$grailsVersion"
-      build ":release:2.0.4"      
     }  
 }
