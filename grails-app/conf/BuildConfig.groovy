37262a52c659837b8ba1facecc97508b5fa2da89
Rob Meidal <rmeidal@triplecreekriver.com>
2014-02-18 14:20:32 -0700

    Updated buildconfig and version number.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,20 +1,36 @@
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+
 grails.project.dependency.resolution = {
-    inherits("global") {}
-    log "warn" 
-    repositories {
-      grailsCentral()
+  // inherit Grails' default dependencies
+  inherits("global") {
+    // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+    // excludes 'ehcache'
+  }
+  log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  checksums true // Whether to verify checksums on resolve
+  legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+  repositories {
+    inherits true
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
+    mavenLocal()
+    mavenCentral()
+  }
+
+  plugins {
+    runtime ":hibernate:$grailsVersion"
+    build ":tomcat:$grailsVersion"
+    build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+      export = false
     }
-    dependencies {}
-      
-    plugins {
-       build(':release:2.0.3', ':rest-client-builder:1.0.2') {
-          export = false
-       }
-    }      
-      
+  }
 }
 
 
