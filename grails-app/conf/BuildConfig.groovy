bb23301686fa09c1f83fecdb1a7502a4ac0a30fd
Rob Meidal <rmeidal@triplecreekriver.com>
2014-02-18 21:47:24 -0700

    Cleaned up buildconfig.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,26 +1,17 @@
 grails.project.work.dir = 'target'
-
 grails.project.dependency.resolution = {
+  inherits 'global'
+  log 'warn'
 
-    inherits 'global'
-    log 'warn'
-
-    repositories {
-        grailsCentral()
-        mavenLocal()
-        mavenCentral()
-    }
+  repositories {
+    grailsCentral()
+    mavenLocal()
+    mavenCentral()
+  }
 
   plugins {
     build ":tomcat:7.0.50"
     runtime ":hibernate:3.6.10.7"
-
-
     build ":release:3.0.1"
-
-
-
-
-
   }
 }
