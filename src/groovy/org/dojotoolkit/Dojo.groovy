3db0574e3ef0ad3320adee91d01c107e6043c310
Rob Meidal <rmeidal@triplecreekriver.com>
2014-02-19 18:11:11 -0700

    Forgot to update the version number in the Dojo.groovy file. Fixing that bug.

--- src/groovy/org/dojotoolkit/Dojo.groovy
+++ src/groovy/org/dojotoolkit/Dojo.groovy
@@ -2,7 +2,7 @@ package org.dojotoolkit
 
 class Dojo {
   static final String version = "1.6.1"
-  static final String pluginVersion = "1.6.1.17"
+  static final String pluginVersion = "1.6.1.18"
   static final String srcHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}-src.zip"
   static final String releaseHref = "http://download.dojotoolkit.org/release-${version}/dojo-release-${version}.zip"
 }
